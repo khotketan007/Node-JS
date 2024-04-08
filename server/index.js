18deb1f5c7f41c31d38077c66db40ecce1b8d486
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-04-08 09:40:57 +0000

    Server Practice

--- server/index.js
+++ server/index.js
@@ -1,5 +1,28 @@
 // Creating the Http request
-const http = require('http')
+const fs = require('fs');
+const http = require('http');
+const { endianness } = require('os');
 
 //  Creating the Server  
-const myuServer = http.createServer((req, res) => {})
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: ${req.url} Request Received\n`
+    fs.appendFile('./log.txt', log, () => {
+        switch (req.url){
+            case '/':
+                res.end("Home Page")
+            break
+            case '/about':
+                res.end("Hello, I am Ketan ")
+            case '/contact-us':
+                res.end("Contact to Ketan")
+            break
+            default:
+                res.end("404 Error Found")
+        }
+        
+    })
+
+})
+
+// Now we can Declaing the port Number
+myServer.listen(7000, () => console.log("Server Started!"))
