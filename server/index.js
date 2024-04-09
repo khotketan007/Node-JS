4da39c012b0ed4034d83c559c02ab56bc3de01ea
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-04-09 06:00:11 +0000

    Update

--- server/index.js
+++ server/index.js
@@ -2,17 +2,23 @@
 const fs = require('fs');
 const http = require('http');
 const { endianness } = require('os');
+const url = require('url')
 
 //  Creating the Server  
 const myServer = http.createServer((req, res) => {
+    if(req.url === "/favicon.ico") return res.end("")
     const log = `${Date.now()}: ${req.url} Request Received\n`
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl);
     fs.appendFile('./log.txt', log, () => {
         switch (req.url){
             case '/':
                 res.end("Home Page")
             break
             case '/about':
-                res.end("Hello, I am Ketan ")
+                const userName = myUrl.query.myname
+                res.end(`Hello, ${userName}`)
+            break
             case '/contact-us':
                 res.end("Contact to Ketan")
             break
