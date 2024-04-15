7904d80502e3bbcdd6c0a6c7794c03861a3cc3a3
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-04-15 07:19:25 +0000

    Bootstrap

---
+++ BootStrap/basci-bootstrap/src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return <div>
+    <h2>Hello Bootstrap Here!</h2>
+    <button type="button" class="btn btn-outline-success">Hello Team!</button>
+  </div>
+}
+
+export default App
