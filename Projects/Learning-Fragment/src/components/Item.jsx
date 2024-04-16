3a413776d8b5b5dd827aee68a38499f4a02b167f
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-04-16 11:20:01 +0000

    Some Styling

--- Projects/Learning-Fragment/src/components/Item.jsx
+++ Projects/Learning-Fragment/src/components/Item.jsx
@@ -1,10 +1,11 @@
+import styles from "./Item.module.css";
 const Item = ({ Fooditem }) => {
   // You can Do also destructuring of the Properties
   // let { foodItem } = props;
   return (
     <>
       <li className="list-group-item">
-        <span className="kk-bold">{Fooditem}</span>
+        <span className={`${styles["kk-bold"]}`}>{Fooditem}</span>
       </li>
     </>
   );
