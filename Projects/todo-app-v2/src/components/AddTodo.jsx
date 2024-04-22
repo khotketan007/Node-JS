91996ec3791e0e5becc0f01fb62bf79ebfac8d58
khotketan007 <khotk29@gmail.com>
2024-04-22 14:15:05 +0530

    Update

--- Projects/todo-app-v2/src/components/AddTodo.jsx
+++ Projects/todo-app-v2/src/components/AddTodo.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+
 // Function Definition
-function AddTodo({onNewItem}) {
+function AddTodo({ onNewItem }) {
+
+  const [todoName, setTodoName] = useState()
+  const [dueDate, setDueDate] = useState()
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  }
+  const handleDateChange = (event) => {
+    setDueDate(event.target.value);
+  }
+
+  const handleAddButtonClick = () => {
+    onNewItem(todoName, dueDate)
+    setDueDate("")
+    setTodoName("")
+  }
+
   return (
     <div className="row Kk-row">
       <div className="col-6">
-        <input type="text" placeholder="Enter Your Work Here"></input>
+        <input type="text" 
+       value={todoName} placeholder="Enter Your Work Here" onChange={handleNameChange}></input>
       </div>
       <div className="col-4">
-        <input type="date"></input>
+        <input type="date" value={dueDate} onChange={handleDateChange}></input>
       </div>
       <div className="col-2">
-        <button type="button" className="btn btn-success Kk-Button" onClick={() => onNewItem('a','b')}>
+        <button type="button" className="btn btn-success Kk-Button" onClick={handleAddButtonClick}>
           Add
         </button>
       </div>
